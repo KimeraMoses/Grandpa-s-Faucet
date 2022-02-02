a1d980230e5fdda5f006bb165bc3e230a7f449ed
Moxhus <kimeramoses001@gmail.com>
2022-02-02 21:41:45 +0300

    New: added the env file

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-import userReducer from "../store/Slices/userSlice";
-import userRegistrationReducer from "../store/Slices/userRegistrationSlice";
 import authReducer from "./Slices/authSlice";
 import transactionReducer from "./Slices/transactionsSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    users: userReducer,
     transactions: transactionReducer,
-    registration: userRegistrationReducer,
   },
 });
 
 export default store;
-export const baseUrl = "https://grandpas-faucet.herokuapp.com";
-export const apiKey ="asdfasdfasdfasdfasfasfasdf";
+
